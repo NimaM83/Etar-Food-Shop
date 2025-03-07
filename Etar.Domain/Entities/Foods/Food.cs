81e6456d0321f2a01dc0f9808eac0d9496f774c6
bikaar <nima.268183@gmail.com>
2025-03-07 04:26:25 -0800

    create food and food category entities

---
+++ Etar.Domain/Entities/Foods/Food.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Etar.Domain.Entities.Foods
+{
+    public class Food : BaseEntity  
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Inventory { get; set; }
+        public  double Price { get; set; }
+
+        public virtual FoodCategory Caategory { get; set; }
+        public Guid CatId { get; set; }
+    
+    }
+}
