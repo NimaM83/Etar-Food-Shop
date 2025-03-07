4196834acafe8045ab2ddf668be93403bb4717da
bikaar <nima.268183@gmail.com>
2025-03-07 09:40:39 -0800

    Category Menu and Create view

---
+++ Etar.WebAdmin/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using Etar.Application.Interfaces.Services.Admin;
+using Etar.Domain.Entities.Foods;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Etar.WebAdmin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly IAdminServices _service;
+        public CategoryController(IAdminServices service)
+        { 
+            _service = service;
+        }
+        public IActionResult Index()
+        {
+            return View(_service.FoodServices.GetCategoriesService.Execute(null));
+        }
+
+        [HttpGet]
+        public IActionResult Create ()
+        {
+            return View(); 
+        }
+
+        [HttpPost]
+        public IActionResult Create(FoodCategory category)
+        {
+            return Json(_service.FoodServices.AddNewCategoryService.Execute(category.Name));
+        }
+    }
+}
